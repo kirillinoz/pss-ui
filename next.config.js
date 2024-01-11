ec0f00825bdc457b8d5fe737f589ed6d41c7ca3e
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2024-01-11 15:49:00 +0100

    Fix :wrench: - Undo redirect

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://[af49:8982:876:51ff::1]:3001/api/:path*",
+        destination: "http://localhost:3001/api/:path*",
+        // destination: "http://[af49:8982:876:51ff::1]:3001/api/:path*", // for ipv6
         permanent: true,
       },
     ];
