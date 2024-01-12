d507c105bd35a73a3a02edf5484a490fed61916b
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2024-01-12 15:14:42 +0100

    Config rewrites fix :fix:

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
+  async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
-        // destination: "http://[af49:8982:876:51ff::1]:3001/api/:path*", // for ipv6
-        permanent: true,
+        //destination: "http://localhost:3001/api/:path*",
+        destination: "http://[af49:8982:876:51ff::1]:3001/api/:path*", // for ipv6
       },
     ];
   },
