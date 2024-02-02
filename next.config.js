ff29d29c8ba3c39af88766437984cbb8d635c70e
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2024-02-02 12:03:00 +0100

    Added headers

--- next.config.js
+++ next.config.js
@@ -4,8 +4,29 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://[af49:8982:876:51ff::1]:3001/api/:path*", // replace 'your-ipv6-address' with your actual IPv6 address
+        destination: "http://localhost:3001/api/:path*", // replace 'your-ipv6-address' with your actual IPv6 address
         permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
