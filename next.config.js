45adcbf692ae1e1b7c2b501fc68cea4431f2b486
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2024-03-01 13:08:07 +0100

    Revert changes to destination in rewrite :wrench:

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://[af49:8982:876:51ff::1]:3001/api/:path*",
+        destination: "http://localhost:3001/api/:path*",
       },
     ];
   },
